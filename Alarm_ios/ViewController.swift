d78e1e0be5323eb30583fa1abb74de0b07e5b08d
shinketya <shinketya@gmail.com>
2018-11-07 17:21:48 +0900

    This is the first commit.

--- Alarm_ios/ViewController.swift
+++ Alarm_ios/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate{
 
+    @IBAction func setNotification(_ sender: Any) {
+        // タイトル、本文、サウンド設定の保持
+        let content = UNMutableNotificationContent()
+        content.title = "時間です"
+        content.subtitle = "10秒経過しました"
+        content.body = "タップしてアプリを開いてください"
+        content.sound = UNNotificationSound.default
+        
+        // seconds後に起動するトリガーを保持
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        // 識別子とともに通知の表示内容とトリガーをrequestに内包する
+        let request = UNNotificationRequest(identifier: "Timer", content: content, trigger: trigger)
+        // UNUserNotificationCenterにrequestを加える
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.add(request) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // フォアグランドの場合でも通知を表示する
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent willPresentnotification: UNNotification, withCompletionHandler: @escaping(UNNotificationPresentationOptions) -> Void) {
+        withCompletionHandler([.alert, .badge, .sound])
     }
 
 
