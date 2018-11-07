715c7d5879c6e50ee90347ad0c2c256a2f5223ce
shinketya <shinketya@gmail.com>
2018-11-07 16:06:56 +0900

    Initial Commit

---
+++ Alarm_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarm_ios
+//
+//  Created by b1017105 on 2018/11/07.
+//  Copyright © 2018年 b1017105. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
